60feec7ed330eb302a58c195506834fd68a81c5e
Sara Baqla <missatrox44@gmail.com>
2024-11-08 07:13:41 -0600

    renamed toggle variable

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -3,21 +3,21 @@
 import { useState } from "react";
 
 const Toggle = () => {
-  const [isOn, setIsOn] = useState(false);
+  const [isEnglish, setIsEnglish] = useState(false);
 
   return (
     <div className="flex items-center space-x-4">
       <button
-        onClick={() => setIsOn(!isOn)}
+        onClick={() => setIsEnglish(!isEnglish)}
         className={`relative w-12 h-6 rounded-full transition-colors duration-300 ease-in-out focus:outline-none ${
-          isOn
+          isEnglish
             ? "bg-gradient-to-r from-[#844AEA] via-[#5C6ADA] to-[#388DD5]"
-            : "bg-gradient-to-r from-[#FD7F00] via-[#FEA800] to-[#FFC800]"
+            : "bg-gradient-to-r from-[#FF7F3F] via-[#FF9A3C] to-[#FFB547]"
         }`}
       >
         <span
           className={`absolute left-0 top-0 h-6 w-6 transform bg-[#E6E6E6] rounded-full shadow-md transition-transform duration-300 ease-in-out ${
-            isOn ? "translate-x-6" : "translate-x-0"
+            isEnglish ? "translate-x-6" : "translate-x-0"
           }`}
         ></span>
       </button>
