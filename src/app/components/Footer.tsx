186a100789fa13a04fdd321ff9202153c968deca
Sara Baqla <missatrox44@gmail.com>
2024-11-08 08:47:12 -0600

    footer text, icon files added

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 const Footer = () => {
 
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="flex items-center space-x-4">
-      <p>Footer</p> 
+    <div className="wrapper flex flex-col items-center">
+      <div className="flex gap-x-4 items-center">
+        <Image src="/logo.png" alt="KD's Paleteria Logo" width={32} height={32} />
+        <p className="font-acme">KD's Paleteria & Snacks</p>
+      </div>
+      <div>Icons</div>
+      <div className="footer-credits"> © {currentYear} | KD's Paleteria & Snacks | <Link target="_blank" href="https://www.sarabaqla.dev/">S4R4</Link></div>
     </div>
   );
 };
