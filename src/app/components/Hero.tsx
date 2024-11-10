57dce8cbc6331af4e2a2fa96c607392971f9d262
Sara Baqla <missatrox44@gmail.com>
2024-11-09 19:08:03 -0600

    logo on hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,11 +3,20 @@ import Image from "next/image";
 const Hero = () => {
 
   return (
-    <div>
+    <div className="relative">
       <video width="1920" height="1080" autoPlay muted>
         <source src="/video/hero-placeholder.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="absolute inset-x-0 bottom-0 transform translate-y-1/2 flex justify-center">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={200}
+          height={200}
+          className=""
+        />
+      </div>
     </div>
   );
 };
