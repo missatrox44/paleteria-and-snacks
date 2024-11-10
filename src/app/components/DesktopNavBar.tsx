872b9025e19dd58607a17332aa2af28bdee984ca
Sara Baqla <missatrox44@gmail.com>
2024-11-09 18:51:18 -0600

    basic navbar

--- src/app/components/DesktopNavBar.tsx
+++ src/app/components/DesktopNavBar.tsx
@@ -1,11 +1,24 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
+import Toggle from "./Toggle";
 const DesktopNavBar = () => {
 
-
   return (
-    <nav className="flex">
-      <h1>DesktopNavBar here</h1>
+    // TODO: make nav sticky and blurry with opacity bg
+    <nav className="absolute top-0 left-0 w-full z-50">
+      <div className="flex wrapper justify-between py-8 items-center">
+        <div className="flex gap-x-6 items-center">
+          <Image src="/logo.png" alt="KD&apos;s Paleteria Logo" width={48} height={48} />
+          <ul className="flex gap-x-6">
+            <li><Link href="#ice-cream">Ice Cream</Link></li>
+            <li><Link href="#popsicles">Popsicles</Link></li>
+            <li><Link href="#snacks">Snacks</Link></li>
+            <li><Link href="#drinks">Drinks</Link></li>
+          </ul>
+        </div>
+        <Toggle />
+      </div>
+     
     </nav>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { Toggle } from "./components"
+
 export default function Home() {
   return (
    <div>
-
-    <Toggle/>
-    <h1 className="font-pacifico text-6xl">Pacifico</h1>
+    {/* <h1 className="font-pacifico text-6xl">Pacifico</h1>
       <p className="font-acme text-2xl">Acme</p>
       <p className="font-afacad text-2xl">Afacad</p>
       <div className="wrapper">
         <p className="text-2xl">Hello, world!</p>
-      </div>
+      </div> */}
     </div>
   );
 }
