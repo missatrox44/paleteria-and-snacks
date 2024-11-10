e038ea6c5b49e8551ae3e3c6fb66aec73bbb9545
Sara Baqla <missatrox44@gmail.com>
2024-11-10 11:27:24 -0600

    add text to toggle

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -6,21 +6,25 @@ const Toggle = () => {
   const [isEnglish, setIsEnglish] = useState(false);
 
   return (
-    <div className="flex items-center space-x-4">
-      <button
-        onClick={() => setIsEnglish(!isEnglish)}
-        className={`relative w-12 h-6 rounded-full transition-colors duration-300 ease-in-out focus:outline-none ${
-          isEnglish
-            ? "bg-gradient-to-r from-[#FF7F3F] via-[#FF9A3C] to-[#FFB547]"
-            : "bg-gradient-to-r from-[#844AEA] via-[#5C6ADA] to-[#388DD5]"
-        }`}
-      >
-        <span
-          className={`absolute left-0 top-0 h-6 w-6 transform bg-[#E6E6E6] rounded-full shadow-md transition-transform duration-300 ease-in-out ${
-            isEnglish ? "translate-x-6" : "translate-x-0"
+    <div className="flex flex-col items-center gap-y-1">
+      <div className="flex items-center space-x-4">
+        <button
+          onClick={() => setIsEnglish(!isEnglish)}
+          className={`relative w-12 h-6 rounded-full transition-colors duration-300 ease-in-out focus:outline-none ${
+            isEnglish
+              ? "bg-gradient-to-r from-[#FF7F3F] via-[#FF9A3C] to-[#FFB547]"
+              : "bg-gradient-to-r from-[#844AEA] via-[#5C6ADA] to-[#388DD5]"
           }`}
-        ></span>
-      </button>
+        >
+          {/* white circle */}
+          <span
+            className={`absolute left-0 top-0 h-6 w-6 transform bg-[#E6E6E6] rounded-full shadow-md transition-transform duration-300 ease-in-out ${
+              isEnglish ? "translate-x-6" : "translate-x-0"
+            }`}
+          ></span>
+        </button>
+      </div>
+      <p>EN/ES</p>
     </div>
   );
 };
