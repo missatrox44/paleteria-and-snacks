e73c450c155d88a4193b4453f98032aedaf73481
Sara Baqla <missatrox44@gmail.com>
2024-11-10 16:12:52 -0600

    horizontal neon bars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero, MenuIcons,LocationHours, FollowSocial } from "@/app/components";
+import Image from "next/image";
 export default function Home() {
   return (
    <div>
@@ -8,7 +9,13 @@ export default function Home() {
     {/* Pocpsicles */}
     {/* Snacks */}
     {/* Drinks */}  
+    <div className="wrapper py-4 md:py-8">
+      <Image src="/neon-bars/purple-neon-slim.svg" alt="Horizontal purple neon bar" width={1920} height={10} />
+    </div>
     <FollowSocial />
+    <div className="wrapper py-4 md:py-8">
+      <Image src="/neon-bars/green-neon-slim.svg" alt="Horizontal green neon bar" width={1920} height={10} />
+    </div>
     <LocationHours />
     
     {/* <h1 className="font-pacifico text-6xl">Pacifico</h1>
