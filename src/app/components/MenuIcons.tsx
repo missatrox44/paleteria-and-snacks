4b2d6a86b15c5c3c4fbf10560e79e52c9c1f514e
Sara Baqla <missatrox44@gmail.com>
2024-11-10 11:03:53 -0600

    menu icons

--- src/app/components/MenuIcons.tsx
+++ src/app/components/MenuIcons.tsx
@@ -4,21 +4,21 @@ const MenuIcons = () => {
  
   return (
     <section className="wrapper grid grid-cols-2 md:grid-cols-4 gap-8 pt-[150px]">
-    <div className="flex flex-col">
-      {/* <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} /> */}
-      <p className="font-acme text-2xl">Ice Cream</p>
+    <div className="flex flex-col items-center gap-y-6">
+      <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} />
+      <p className="font-acme text-base md:text-2xl">Ice Cream</p>
     </div>
-    <div className="flex flex-col">
-      {/* <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} /> */}
-      <p className="font-acme text-2xl">Popsicles</p>
+    <div className="flex flex-col items-center gap-y-6">
+      <Image src="/icons/popsicle.svg" alt="Popsicle Icon" width={64} height={64} />
+      <p className="font-acme text-base md:text-2xl">Popsicles</p>
     </div>
-    <div className="flex flex-col">
-      {/* <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} /> */}
-      <p className="font-acme text-2xl">Snacks</p>
+    <div className="flex flex-col items-center gap-y-6">
+      <Image src="/icons/snack.svg" alt="Snack Icon" width={64} height={64} />
+      <p className="font-acme text-base md:text-2xl">Snacks</p>
     </div>
-    <div className="flex flex-col">
-      {/* <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} /> */}
-      <p className="font-acme text-2xl">Drinks</p>
+    <div className="flex flex-col items-center gap-y-6">
+      <Image src="/icons/drinks.svg" alt="Drinks Icon" width={64} height={64} />
+      <p className="font-acme text-base md:text-2xl">Drinks</p>
     </div>
     </section>
   );
