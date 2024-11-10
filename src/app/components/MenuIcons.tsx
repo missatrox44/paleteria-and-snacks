a69e00dff7b314cdbb113973e86659bbc5fd2f98
Sara Baqla <missatrox44@gmail.com>
2024-11-10 11:22:39 -0600

    menu icons animation

--- src/app/components/MenuIcons.tsx
+++ src/app/components/MenuIcons.tsx
@@ -4,25 +4,25 @@ const MenuIcons = () => {
  
   return (
     <section className="wrapper grid grid-cols-2 md:grid-cols-4 gap-8 pt-[150px]">
-      <Link href="#ice-cream">
+      <Link href="#ice-cream" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
           <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} />
           <p className="font-acme text-base md:text-2xl">Ice Cream</p>
         </div>
       </Link>
-      <Link href="#popsicles">
+      <Link href="#popsicles" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
           <Image src="/icons/popsicle.svg" alt="Popsicle Icon" width={64} height={64} />
           <p className="font-acme text-base md:text-2xl">Popsicles</p>
         </div>
       </Link>
-      <Link href="#snacks">
+      <Link href="#snacks" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
           <Image src="/icons/snack.svg" alt="Snack Icon" width={64} height={64} />
           <p className="font-acme text-base md:text-2xl">Snacks</p>
         </div>
       </Link>
-      <Link href="#drinks">
+      <Link href="#drinks" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
           <Image src="/icons/drinks.svg" alt="Drinks Icon" width={64} height={64} />
           <p className="font-acme text-base md:text-2xl">Drinks</p>
