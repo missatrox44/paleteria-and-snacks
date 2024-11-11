7e771f82344cfbaee16d4f86e134c312dfae6e0c
Sara Baqla <missatrox44@gmail.com>
2024-11-11 17:04:37 -0600

    food category pb-10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,45 +7,50 @@ export default function Home() {
       <Hero />
       <MenuIcons />
       {/* IceCream */}
-      <FoodCategory
-        id="ice-cream"
-        neonBar="/neon-bars/pink-neon-vertical2.svg"
-        imgSrc="/icons/ice-cream.svg"
-        categoryTitle="Ice Cream"
-        categoryDescription="This is the description of the ice cream at the store."
-        foodItems={iceCreamData}
-        classes="text-neon-pink pink-neon-glow"
-      />
-      {/* Pocpsicles */}
-      <FoodCategory
-        id="popsicles"
-        neonBar="/neon-bars/turquoise-neon-vertical2.svg"
-        imgSrc="/icons/popsicle.svg"
-        categoryTitle="Popsicles"
-        categoryDescription="This is the description of the popsicles at the store."
-        foodItems={popsiclesData}
-        classes="text-neon-turquoise turquoise-neon-glow"
-      />
-      {/* Snacks */}
-      <FoodCategory
-        id="snacks"
-        neonBar="/neon-bars/orange-neon-vertical2.svg"
-        imgSrc="/icons/snack.svg"
-        categoryTitle="Snacks"
-        categoryDescription="This is the description of the snacks at the store."
-        foodItems={snacksData}
-        classes="text-neon-orange orange-neon-glow"
-      />
-      {/* Drinks */}
-      <FoodCategory
-        id="drinks"
-        neonBar="/neon-bars/green-neon-vertical2.svg"
-        imgSrc="/icons/drinks.svg"
-        categoryTitle="Drinks"
-        categoryDescription="This is the description of the drinks at the store."
-        foodItems={drinksData}
-        classes="text-neon-green green-neon-glow"
-      />
+      <div className="pb-10">
+        <FoodCategory
+          id="ice-cream"
+          neonBar="/neon-bars/pink-neon-vertical2.svg"
+          imgSrc="/icons/ice-cream.svg"
+          categoryTitle="Ice Cream"
+          categoryDescription="This is the description of the ice cream at the store."
+          foodItems={iceCreamData}
+          classes="text-neon-pink pink-neon-glow"
+        />
+      </div>
+      <div className="pb-10">
+        <FoodCategory
+          id="popsicles"
+          neonBar="/neon-bars/turquoise-neon-vertical2.svg"
+          imgSrc="/icons/popsicle.svg"
+          categoryTitle="Popsicles"
+          categoryDescription="This is the description of the popsicles at the store."
+          foodItems={popsiclesData}
+          classes="text-neon-turquoise turquoise-neon-glow"
+        />
+      </div>
+      <div className="pb-10">
+        <FoodCategory
+          id="snacks"
+          neonBar="/neon-bars/orange-neon-vertical2.svg"
+          imgSrc="/icons/snack.svg"
+          categoryTitle="Snacks"
+          categoryDescription="This is the description of the snacks at the store."
+          foodItems={snacksData}
+          classes="text-neon-orange orange-neon-glow"
+        />
+      </div>
+      <div className="pb-10">
+        <FoodCategory
+          id="drinks"
+          neonBar="/neon-bars/green-neon-vertical2.svg"
+          imgSrc="/icons/drinks.svg"
+          categoryTitle="Drinks"
+          categoryDescription="This is the description of the drinks at the store."
+          foodItems={drinksData}
+          classes="text-neon-green green-neon-glow"
+        />
+      </div>
 
 
       <div className="wrapper py-4 md:py-8">
