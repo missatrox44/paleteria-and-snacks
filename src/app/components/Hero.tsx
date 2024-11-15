b779ba88fcc72c70f0d0f5a61cb295d2dc14a765
Sara Baqla <missatrox44@gmail.com>
2024-11-15 08:49:13 -0600

    dynamic logo size

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,11 +8,14 @@ const Hero = () => {
   const size = useWindowSize();
 
   let videoSrc = "/video/desktop-video.mp4";
+  let logoSize = 250;
 
   if (size.width && size.width < 768) {
     videoSrc = "/video/mobile-video.mp4";
+    logoSize = 125;
   } else if (size.width && size.width < 1024) {
     videoSrc = "/video/tablet-video.mp4";
+    logoSize = 175;
   }
 
   return (
@@ -25,8 +28,8 @@ const Hero = () => {
         <Image
           src="/logo.png"
           alt="Logo"
-          width={200}
-          height={200}
+          width={logoSize}
+          height={logoSize}
           className=""
         />
       </div>
