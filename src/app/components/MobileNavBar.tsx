2c8540b6512a8bec67aa32d91afab59eca4960ec
Sara Baqla <missatrox44@gmail.com>
2024-11-16 22:00:51 -0600

    mobile menu full height bug fixed

--- src/app/components/MobileNavBar.tsx
+++ src/app/components/MobileNavBar.tsx
@@ -10,17 +10,14 @@ const MobileNavBar = () => {
   const scrolled = useScrolled();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
 
-  // ${scrolled ? "bg-black/30 backdrop-blur-md" : ""
-  // }
-
   return (
     <nav
-      className={`sm:hidden fixed top-0 left-0 w-full z-50 transition-all duration-300`}
+      className={`sm:hidden fixed top-0 left-0 w-full z-50 transition-all duration-300 ${scrolled ? "bg-black/30 backdrop-blur-md" : ""
+        }`}
     >
       <div className="flex justify-between p-3 items-center">
         <button
@@ -37,12 +34,13 @@ const MobileNavBar = () => {
 
         {/* Drawer Menu */}
         <div
-          className={`fixed top-0 left-0 h-full w-1/2 bg-gradient-to-t from-purple-500 to-orange-500 shadow-lg transform transition-transform duration-300 z-[999] ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          className={`fixed top-0 left-0 h-screen w-1/2 bg-gradient-to-t from-purple-500 to-orange-500 shadow-lg transform transition-transform duration-300 z-[999] ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
             }`}
         >
           <div className="flex flex-col h-full">
+            {/* Header with Logo and Close Button */}
             <div className="flex justify-between pt-3 px-3 pb-6">
-            <div className="">
+              <div>
                 <Image src="/logo.png" alt="Logo" width={35} height={35} />
               </div>
               <button
@@ -52,40 +50,28 @@ const MobileNavBar = () => {
               >
                 <Image src="/icons/cross.svg" alt="Close Menu" width={20} height={20} />
               </button>
-
             </div>
-            {/* <div className="flex items-center justify-center py-6">
-                <Image src="/logo.png" alt="Logo" width={60} height={60} />
-              </div> */}
+
             {/* Navigation Links */}
             <ul className="flex flex-col gap-y-4 text-lg font-medium pl-4">
               <li onClick={toggleMenu}>
-                <Link className="" href="#ice-cream">
-                  Ice Cream
-                </Link>
+                <Link href="#ice-cream">Ice Cream</Link>
               </li>
-             <li onClick={toggleMenu}>
-                <Link className="" href="#popsicles">
-                  Popsicles
-                </Link>
+              <li onClick={toggleMenu}>
+                <Link href="#popsicles">Popsicles</Link>
               </li>
-             <li onClick={toggleMenu}>
-                <Link className="" href="#snacks">
-                  Snacks
-                </Link>
+              <li onClick={toggleMenu}>
+                <Link href="#snacks">Snacks</Link>
               </li>
-             <li onClick={toggleMenu}>
-                <Link className="" href="#drinks">
-                  Drinks
-                </Link>
+              <li onClick={toggleMenu}>
+                <Link href="#drinks">Drinks</Link>
               </li>
-             <li onClick={toggleMenu}>
-                <Link className="" href="#hours-location">
-                  Hours & Location
-                </Link>
+              <li onClick={toggleMenu}>
+                <Link href="#hours-location">Hours & Location</Link>
               </li>
             </ul>
 
+            {/* Toggle Component */}
             <div className="mt-auto p-6">
               <Toggle />
             </div>
