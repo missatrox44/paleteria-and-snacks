9a419ef6ee1aee1a4a483801872321fbc7d38570
Sara Baqla <missatrox44@gmail.com>
2024-11-17 18:39:42 -0600

    mobile toggle in fixed menu

--- src/app/components/MenuIcons.tsx
+++ src/app/components/MenuIcons.tsx
@@ -3,29 +3,29 @@ import Link from "next/link";
 const MenuIcons = () => {
  
   return (
-    <section className="wrapper grid grid-cols-2 md:grid-cols-4 gap-8 pt-[90px] md:pt-[150px] pb-[55px]">
+    <section className="wrapper grid grid-cols-2 md:grid-cols-4 gap-y-14 md:gap-8 pt-[90px] md:pt-[150px] pb-[55px]">
       <Link href="#ice-cream" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
-          <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={64} height={64} />
-          <p className="font-acme text-base md:text-2xl">Ice Cream</p>
+          <Image src="/icons/ice-cream.svg" alt="Ice Cream Icon" width={85} height={85} />
+          <p className="font-acme text-xl md:text-2xl">Ice Cream</p>
         </div>
       </Link>
       <Link href="#popsicles" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
-          <Image src="/icons/popsicle.svg" alt="Popsicle Icon" width={64} height={64} />
-          <p className="font-acme text-base md:text-2xl">Popsicles</p>
+          <Image src="/icons/popsicle.svg" alt="Popsicle Icon" width={85} height={85} />
+          <p className="font-acme text-xl md:text-2xl">Popsicles</p>
         </div>
       </Link>
       <Link href="#snacks" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
-          <Image src="/icons/snack.svg" alt="Snack Icon" width={64} height={64} />
-          <p className="font-acme text-base md:text-2xl">Snacks</p>
+          <Image src="/icons/snack.svg" alt="Snack Icon" width={85} height={85} />
+          <p className="font-acme text-xl md:text-2xl">Snacks</p>
         </div>
       </Link>
       <Link href="#drinks" className="menu-icon">
         <div className="flex flex-col items-center gap-y-6">
-          <Image src="/icons/drinks.svg" alt="Drinks Icon" width={64} height={64} />
-          <p className="font-acme text-base md:text-2xl">Drinks</p>
+          <Image src="/icons/drinks.svg" alt="Drinks Icon" width={85} height={85} />
+          <p className="font-acme text-xl md:text-2xl">Drinks</p>
         </div>
       </Link>
     </section>
--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -24,7 +24,7 @@ const Toggle = () => {
           ></span>
         </button>
       </div>
-      <p>EN/ES</p>
+      <p className="text-sm ">EN/ES</p>
     </div>
   );
 };
