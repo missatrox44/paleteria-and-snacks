9ec5845bacd55702b76ac03bf88fb559290a8f92
Sara Baqla <missatrox44@gmail.com>
2024-11-19 17:43:05 -0600

    english spanish toggle text color;

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -24,22 +24,24 @@ const Toggle = () => {
       <div className="flex items-center space-x-4">
         <button
           onClick={toggleLanguage}
-          className={`relative w-12 h-6 rounded-full transition-colors duration-300 ease-in-out ${
-            isEnglish
+          className={`relative w-12 h-6 rounded-full transition-colors duration-300 ease-in-out ${isEnglish
               ? "bg-gradient-to-r from-[#FF7F3F] via-[#FF9A3C] to-[#FFB547]"
               : "bg-gradient-to-r from-[#844AEA] via-[#5C6ADA] to-[#388DD5]"
-          }`}
+            }`}
         >
           {/* white circle */}
           <span
-            className={`absolute left-0 top-0 h-6 w-6 transform bg-[#E6E6E6] rounded-full shadow-md transition-transform duration-300 ease-in-out ${
-              isEnglish ? "translate-x-6" : "translate-x-0"
-            }`}
+            className={`absolute left-0 top-0 h-6 w-6 transform bg-[#E6E6E6] rounded-full shadow-md transition-transform duration-300 ease-in-out ${isEnglish ? "translate-x-6" : "translate-x-0"
+              }`}
           ></span>
         </button>
       </div>
-      <p className="text-sm">EN/ES</p>
-      <p>{isEnglish ? "English" : "Español"}</p>
+
+      <p className="text-sm font-bold">
+        <span className={isEnglish ? "text-[#FF7F3F]" : ""}>EN</span>/
+        <span className={!isEnglish ? "text-[#844AEA]" : ""}>ES</span>
+      </p>
+
     </div>
   );
 };
