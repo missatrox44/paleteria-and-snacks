29457548ea50202164541a3ef22966a22350392f
Sara Baqla <missatrox44@gmail.com>
2024-11-19 22:44:09 -0600

    hours translations

---
+++ src/app/translations/contact.ts
@@ -0,0 +1,13 @@
+export const contactText = {
+  "en": [
+    "Hours & Location",
+    "Sunday - Thursday: 1pm - 9pm",
+    "Friday - Saturday: 1pm - 10pm"
+
+  ],
+  "es": [
+    "Horarios y Ubicación",
+    "Domingo - Jueves: 1pm - 9pm",
+    "Viernes - Sábado: 1pm - 10pm",
+  ],
+};
